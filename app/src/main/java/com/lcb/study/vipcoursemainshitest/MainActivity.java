ac44218b509f2c67ca897d5bae250790d4d48b91
lichangbin008 <lichangbin008@163.com>
2020-08-24 15:42:38 +0800

    完成自定义view，开始自定义ViewGroup

--- app/src/main/java/com/lcb/study/vipcoursemainshitest/MainActivity.java
+++ app/src/main/java/com/lcb/study/vipcoursemainshitest/MainActivity.java
@@ -2,13 +2,34 @@ package com.lcb.study.vipcoursemainshitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lcb.study.vipcoursemainshitest.ui.UIActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btUi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btUi=findViewById(R.id.bt_ui);
+
+        initListener();
+    }
+
+    private void initListener() {
+        btUi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UIActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/lcb/study/vipcoursemainshitest/ui/MNViewGroup.java
@@ -0,0 +1,28 @@
+package com.lcb.study.vipcoursemainshitest.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by ${lichangbin} on 2020/8/24.
+ */
+public class MNViewGroup extends ViewGroup {
+
+    public MNViewGroup(Context context) {
+        super(context);
+    }
+
+    public MNViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MNViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
